84fe2861ffe86484f54ae08e8a003fd2cd4e7f8a
nickolaykondratyev <nickolay.kondratyev@gmail.com>
2026-03-19 08:03:56 -0600

    Modified file: test_post_ticket_consume.sh

--- test_post_ticket_consume.sh
+++ test_post_ticket_consume.sh
@@ -5,14 +5,30 @@ main() {
   cdi.repo_root
 
   mkdir -p ./.tmp/
-  ./run_sonar.sh &> "./.tmp/sonar_run_$(date_now_file_friendly).txt"
+  local sonar_out="./.tmp/sonar_run_$(date_now_file_friendly).txt"
+  local test_out="./.tmp/test_run_$(date_now_file_friendly).txt"
+
+  local failures=()
+
+
+  ./test.sh &> ${test_out:?} || {
+    failures+=("[./test.sh] - FAILED to run. Need to ROOT cause and fix. (Output in [${test_out:?}])")
+  }
+
+  ./run_sonar.sh &> "${sonar_out:?}" || {
+    failures+=("[./run_sonar.sh] - FAILED to run. Need to ROOT cause and fix. (Output in [${sonar_out:?}])")
+  }
 
   if cat ./_reports/sonar_issues.jsonl | lines.count_greater_than 0; then
-    echo "There are issues found in [./_reports/sonar_issues.jsonl]"
-    return 1
+    failures+=("[./run_sonar.sh] - There are issues found in [./_reports/sonar_issues.jsonl] Need to root cause and fix.")
   fi
 
-  return 0
+  if [[ ${#failures[@]} -eq 0 ]]; then
+      return 0
+  else
+    array_print "${failures[@]}"
+    return 1
+  fi
 }
 
 main "${@}"
