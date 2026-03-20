a05b0117ac12ef08e994e8880373bc20a8223860
CC_sonnet-v4.6_WITH-nickolaykondratyev <nickolay.kondratyev@gmail.com>
2026-03-19 18:15:02 -0600

    Modified file: run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 
 main() {
   ./gradlew :app:installDist
+  echo "--------------------------------------------------------------------------------"
+  echo "Starting ticket run..."
   # Runs the main function at ref.ap.4JVSSyLwZXop6hWiJNYevFQX.E
-  ./app/build/install/app/bin/app
+  ./app/build/install/app/bin/app "${@:-}"
 }
 
 main "${@}"
